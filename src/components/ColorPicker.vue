82d4ea36c29b2bce7d4c8c354114f454e507f297
Moin Iqbal <moiniqbal205@gmail.com>
2021-02-21 16:27:40 +1100

    1st commit: Vue Color Picker

---
+++ src/components/ColorPicker.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="color-picker">
+        <div 
+            class="color" 
+            v-for="(color, i) in colors" 
+            :key="i"
+            :style="'background:'+color"
+            @click="setColor(color)"
+        >
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'color-picker',
+    props: ['colors'],
+    methods: {
+        setColor (color) {
+            this.$emit('setColor', color);
+        }
+    }
+}
+</script>
+
+<style scoped>
+.color-picker {
+    display: grid;
+    width: 125px;
+    margin: 15px auto;
+    grid-template-columns: repeat(5, 25px);
+}
+
+.color {
+    border: 1px solid #AAA;
+    height: 25px;
+}
+</style>
